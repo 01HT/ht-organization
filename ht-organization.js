59fc9f489536ec8c8fb52655040d8f76038092c4
maksimyurkov <yurkov.maksim.sergeevich@gmail.com>
2019-08-06 20:10:27 +0300

    change cloudinary url variable

--- ht-organization.js
+++ ht-organization.js
@@ -218,7 +218,9 @@ class HTOrganization extends LitElement {
               ? html`<a href="${
                   orgData.website
                 }" target="_blank" rel="noopener nofollow">
-            <iron-icon src="https://res.cloudinary.com/cdn-01ht/image/upload/v1532588175/logos/website/website-color.svg"></iron-icon>
+            <iron-icon src="${
+              window.appConfig.cloudinary.url
+            }/image/upload/v1532588175/logos/website/website-color.svg"></iron-icon>
             <paper-tooltip position="right" animation-delay="0" offset="4">Сайт пользователя</paper-tooltip>
           </a>`
               : ""
@@ -228,7 +230,9 @@ class HTOrganization extends LitElement {
               ? html`<a href="${
                   orgData.twitter
                 }" target="_blank" rel="noopener nofollow">
-            <iron-icon src="https://res.cloudinary.com/cdn-01ht/image/upload/v1532587138/logos/twitter/twitter-color.svg"></iron-icon>
+            <iron-icon src="${
+              window.appConfig.cloudinary.url
+            }/image/upload/v1532587138/logos/twitter/twitter-color.svg"></iron-icon>
             <paper-tooltip position="right" animation-delay="0" offset="4">Профайл Twitter</paper-tooltip>
           </a>`
               : ""
@@ -238,7 +242,9 @@ class HTOrganization extends LitElement {
               ? html`<a href="${
                   orgData.facebook
                 }" target="_blank" rel="noopener nofollow">
-            <iron-icon src="https://res.cloudinary.com/cdn-01ht/image/upload/v1532586978/logos/facebook/logo-color.svg"></iron-icon>
+            <iron-icon src="${
+              window.appConfig.cloudinary.url
+            }/image/upload/v1532586978/logos/facebook/logo-color.svg"></iron-icon>
             <paper-tooltip position="right" animation-delay="0" offset="4">Профайл Facebook</paper-tooltip>
           </a>`
               : ""
@@ -248,7 +254,9 @@ class HTOrganization extends LitElement {
                ? html`<a href="${
                    orgData.google
                  }" target="_blank" rel="noopener nofollow">
-            <iron-icon src="https://res.cloudinary.com/cdn-01ht/image/upload/v1532600717/logos/google/google-plus.svg"></iron-icon>
+            <iron-icon src="${
+              window.appConfig.cloudinary.url
+            }/image/upload/v1532600717/logos/google/google-plus.svg"></iron-icon>
             <paper-tooltip position="right" animation-delay="0" offset="4">Профайл Google+</paper-tooltip>
           </a>`
                : ""
@@ -258,7 +266,9 @@ class HTOrganization extends LitElement {
               ? html`<a href="${
                   orgData.github
                 }" target="_blank" rel="noopener nofollow">
-            <iron-icon src="https://res.cloudinary.com/cdn-01ht/image/upload/v1532587414/logos/github/github-color.svg"></iron-icon>
+            <iron-icon src="${
+              window.appConfig.cloudinary.url
+            }/image/upload/v1532587414/logos/github/github-color.svg"></iron-icon>
             <paper-tooltip position="right" animation-delay="0" offset="4">Профайл GitHub</paper-tooltip>
           </a>`
               : ""
@@ -371,7 +381,7 @@ class HTOrganization extends LitElement {
         this.page === "about"
           ? `${this.orgData.displayName} | Профайл на Elements`
           : `${this.orgData.displayName} - Портфолио | Elements`,
-      image: `${cloudinaryURL}/c_scale,f_auto,h_512,w_512/v${
+      image: `${window.appConfig.cloudinary.url}/c_scale,f_auto,h_512,w_512/v${
         this.orgData.avatar.version
       }/${this.orgData.avatar.public_id}.png`,
       imageAlt: `${this.orgData.displayName}`,
